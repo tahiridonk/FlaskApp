db6e64173c8f934f4e159126a64117f2807cd49d
Votre Nom <vous@exemple.com>
2025-12-10 19:24:46 +0100

    ULTIMATE FIX: All files corrected for CI/CD

--- test_app.py
+++ test_app.py
@@ -1,8 +1,8 @@
 ﻿import pytest
 from app import app
 
-def test_hello():
-    client = app.test_client()
-    response = client.get("/")
-    assert response.status_code == 200
-    assert b"Hello, World!" in response.data
+def test_index():
+    with app.test_client() as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert b"Hello, World!" in response.data
